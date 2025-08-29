f2d58cb59db75c444c5c5efde8b9b8e1219defa9
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-08-29 21:11:22 +0800

    update hdb schedules

--- dagster_dbt_integration/dagster_dbt_integration/schedules.py
+++ dagster_dbt_integration/dagster_dbt_integration/schedules.py
@@ -2,14 +2,20 @@
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
 from dagster_dbt import build_schedule_from_dbt_selection
+from dagster import ScheduleDefinition, define_asset_job
+from .assets import dbt_resale_dbt_assets, pipeline_meltano
 
-from .assets import dbt_resale_dbt_assets
+# Create a job that includes both assets
+etl_job = define_asset_job(
+    name="materialize_elt",
+    selection=[pipeline_meltano, dbt_resale_dbt_assets]
+)
 
+# Create schedule for the job
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [dbt_resale_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+    ScheduleDefinition(
+        job=etl_job,
+        cron_schedule="0 6 5 * *",  
+        name="monthly_etl_schedule"
+    )
 ]
